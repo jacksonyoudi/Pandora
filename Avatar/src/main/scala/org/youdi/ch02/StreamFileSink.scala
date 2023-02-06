45707699d540cc6ca548fa9dd3a3e1821f3ff129
jacksonyoudi <liangchangyoujackson@gmail.com>
2023-02-06 23:04:05 +0800

    custom sink

--- Avatar/src/main/scala/org/youdi/ch02/StreamFileSink.scala
+++ Avatar/src/main/scala/org/youdi/ch02/StreamFileSink.scala
@@ -1,9 +1,17 @@
 package org.youdi.ch02
 
+import org.apache.avro.{Schema, SchemaBuilder}
+import org.apache.avro.generic.{GenericData, GenericRecord}
 import org.apache.flink.api.common.serialization.{SimpleStringEncoder, SimpleStringSchema}
 import org.apache.flink.connector.file.sink.FileSink
 import org.apache.flink.core.fs.Path
+import org.apache.flink.formats.avro.typeutils.GenericRecordAvroTypeInfo
+import org.apache.flink.formats.parquet.ParquetWriterFactory
+import org.apache.flink.formats.parquet.avro.ParquetAvroWriters
 import org.apache.flink.streaming.api.CheckpointingMode
+import org.apache.flink.streaming.api.functions.sink.filesystem.OutputFileConfig
+import org.apache.flink.streaming.api.functions.sink.filesystem.bucketassigners.DateTimeBucketAssigner
+import org.apache.flink.streaming.api.functions.sink.filesystem.rollingpolicies.DefaultRollingPolicy
 import org.apache.flink.streaming.api.scala._
 
 /**
@@ -25,9 +33,59 @@ object StreamFileSink {
 
     val ds: DataStream[String] = env.addSource(MyParallelSource)
 
-    FileSink.forRowFormat(new Path("hdfs://xxx"), new SimpleStringEncoder[String]("utf-8"))
+    val fileSink: FileSink[String] = FileSink
+      //  文件格式
+      .forRowFormat(new Path("hdfs://xxx"), new SimpleStringEncoder[String]("utf-8"))
+      // 滚动策略
+      .withRollingPolicy(
+        DefaultRollingPolicy.builder
+          .withRolloverInterval(1000)
+          .withMaxPartSize(5 * 1024 * 1024)
+          .build
+      )
+      // 分桶策略
+      .withBucketAssigner(
+        new DateTimeBucketAssigner[String]()
+      )
+      .withBucketCheckInterval(5)
+      .withOutputFileConfig(
+        OutputFileConfig
+          .builder
+          .withPartPrefix("youdi")
+          .withPartSuffix(".txt")
+          .build
+      )
+      .build
 
 
+    // //.addSink()  /* SinkFunction实现类对象,用addSink() 来添加*/
+    //
+    //    ds.addSink(fileSink)
+    ds.sinkTo(fileSink) /*Sink 的实现类对象,用 sinkTo()来添加  */
+
+
+    val schema: Schema = Schema.createRecord("id", "user id", "com.youdi.User", true)
+    val writeFactory0: ParquetWriterFactory[GenericRecord] = ParquetAvroWriters.forGenericRecord(schema)
+
+    val schema1: Schema = SchemaBuilder
+      .builder
+      .record("com.youdi.avro.schema")
+      .fields
+      .requiredLong("uid")
+      .name("info")
+      .`type`()
+      .map
+      .values
+      .`type`("string")
+      .noDefault
+      .endRecord
+
+
+    val record: GenericData.Record = new GenericData.Record(schema)
+    new GenericRecordAvroTypeInfo(schema)
+
+
+    ParquetAvroWriters.forSpecificRecord(AvroEcen)
 
 
 
