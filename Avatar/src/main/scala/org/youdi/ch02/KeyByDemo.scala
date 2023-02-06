aeb8bfa9bf46fe8f177da6b1edf00ce74bd45f22
youdi <liangchangyoujackson@gmail.com>
2023-02-06 10:15:15 +0800

    filter function

---
+++ Avatar/src/main/scala/org/youdi/ch02/KeyByDemo.scala
@@ -0,0 +1,20 @@
+package org.youdi.ch02
+import org.apache.flink.streaming.api.scala._
+
+object KeyByDemo {
+  def main(args: Array[String]): Unit = {
+    val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+    val ds: DataStream[String] = env.socketTextStream("localhost", 9998)
+
+    ds.map(
+      (line: String) => {
+        val arr: Array[String] = line.split(" ")
+        
+      }
+    )
+
+
+
+    env.execute("keyby demo")
+  }
+}
---
+++ Avatar/src/main/scala/org/youdi/ch02/User.scala
@@ -0,0 +1,3 @@
+package org.youdi.ch02
+
+case class User(id: Long, name: String, address: String)
